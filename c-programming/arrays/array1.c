8d21e07dfc97fa89aa8b7349f56f176f7a4e42a1
brightdaniel <ivquestservices@gmail.com>
2023-05-27 22:06:46 +0100

    arrays

---
+++ c-programming/arrays/array1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int age1 = 10; 
+
+	
+
+	int ages[] = { 10, 11, 12, 13, 12, 6, 10 };
+// index numbers	0   1	2   3   4   5   6   7   8   9  10 
+
+	for (int i =  0; i < 11; i++)
+		 printf("%d \n", ages[i]);
+
+
+
+
+
+//	printf("The value of index 5 is  %d \n", ages[]);
+
+
+
+
+
+
+}
